850e8a771729e1669771be48792f870bc9bdb0be
unknowndonthacker <67613775+unknowndonthacker@users.noreply.github.com>
2020-12-12 10:23:33 -0300

    Update squid-proxy-docker.sh

--- squid-proxy-docker.sh
+++ squid-proxy-docker.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run -it -p 80:80 -p 443:443 -p 8080:8080 -p 3128 -v squid-docker-limbo squid-limbo /bin/bash
+docker run -it -p 8080:8080 -v squid-docker-limbo squid-limbo /bin/bash
