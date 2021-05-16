3cb5dae14119427b5c2fe618093f66d606df759e
unknowndonthacker <67613775+unknowndonthacker@users.noreply.github.com>
2021-05-15 23:51:40 -0300

    Update squid-proxy-docker.sh

--- squid-proxy-docker.sh
+++ squid-proxy-docker.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run -it -p 8080:8080 squid-limbo /bin/bash -c "service squid start && apt-get update && htop"
+docker run -it -p 8080:8080 squid-limbo /bin/bash -c "echo loading && apt-get update && apt-get upgrade -y && service squid start && htop"
