8202e1e0cd57fcafb7678c2cf0c8c8742938e1f1
unknowndonthacker <67613775+unknowndonthacker@users.noreply.github.com>
2021-05-19 14:34:51 -0300

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,6 @@
+sudo apt-get install docker git -y
+git clone https://github.com/unknowndonthacker/Squid-Docker.git
+cd Squid-Docker
+docker image build -t squid-limbo .
+chmod +x squid-proxy-docker.sh
+./squid-proxy-docker.sh
