e0d455cbff6aae790c261803b5e5cd74afca7f36
ketubadass <ketubadass@gmail.com>
2021-09-03 16:15:49 -0300

    Update squid-proxy-docker.sh

--- squid-proxy-docker.sh
+++ squid-proxy-docker.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run -it -p 8080:8080 squid-limbo /bin/bash -c "echo loading && apt-get update && apt-get upgrade -y && service squid start && htop"
+docker run -d --restart=always --name=squid-docker -p 8080:8080 --dns-opt='options single-request' --sysctl net.ipv6.conf.all.disable_ipv6=1 squid-limbo /bin/bash -c "echo loading && apt-get update && apt-get upgrade -y && service squid start && service squid status && service squid start && tail -f /var/log/squid/access.log | grep http"
